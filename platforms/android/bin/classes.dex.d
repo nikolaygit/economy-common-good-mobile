e3c08ccae02b93f711c32b7c752b5626f1d24b59
Nikolay Georgiev <nikolay@georgiev.cc>
2014-02-01 18:11:43 +0100

    Fixes #2 - first try with Collapsibles

--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -1,9 +1,10 @@
 /home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes.dex : \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/R$string.class \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/R.class \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/BuildConfig.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/R$string.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/R.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/BuildConfig.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/EconomyForTheommonGood.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/R$attr.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/R$xml.class \
+/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/com/example/hello/R$drawable.class \
 /home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/EconomyForTheommonGood.class \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/R$attr.class \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/R$xml.class \
-/home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/classes/org/gemeinwohloekonomie/R$drawable.class \
 /home/nikolayhg/dev/workspaces/pycharm/economy-common-good-mobile/platforms/android/bin/dexedLibs/classes-b707ff0ffc1eb94a699b392550cf5b87.jar \
--- platforms/android/gen/org/gemeinwohloekonomie/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.gemeinwohloekonomie;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
